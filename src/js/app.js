31d9b89132905fa936de51e2267f39b17991e9bf
Victor Hoang <vic.hoang@yahoo.com>
2015-10-24 19:10:36 -0700

    Get data from local JSON

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,40 @@
 var Vue = require('vue');
 
-new Vue({
+Vue.config.debug = true;
+
+var app = new Vue({
   el: '#app',
+
   data: {
-    message: 'Hello derp!'
+    quotes: []
+  },
+
+  created: function() {
+    this.getData('data.json', this.handleSuccess, this.handleError);
+  },
+
+  methods: {
+    getData: function(path, success, error) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            if (success)
+              success(JSON.parse(xhr.responseText));
+          } else {
+            if (error)
+              error(xhr);
+          }
+        }
+      };
+      xhr.open("GET", path, true);
+      xhr.send();
+    },
+    handleSuccess: function(data) {
+      this.quotes = data;
+    },
+    handleError: function(xhr) {
+      console.log(xhr);
+    }
   }
 });
